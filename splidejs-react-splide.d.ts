f00fd7e5c3dd342a7b12ea375ee8e8f93f3f704c
Maged Faiz <magedfaiz98@gmail.com>
2023-11-24 17:31:17 +0200

    feat: added hero image slider with images

---
+++ splidejs-react-splide.d.ts
@@ -0,0 +1,6 @@
+declare module '@splidejs/react-splide' {
+  const Splide: any;
+  const SplideSlide: any;
+  const SplideTrack: any;
+  export { Splide, SplideSlide, SplideTrack };
+}
---
+++ src/components/hero/ImageSlider.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import img from '/public/imgs/hero/1.jpg';
+import img2 from '/public/imgs/hero/2.jpg';
+import img3 from '/public/imgs/hero/3.jpg';
+import img4 from '/public/imgs/hero/4.jpg';
+
+import '@splidejs/react-splide/css';
+
+const images = [
+  {
+    title: 'Slide Image 1',
+    image: img,
+  },
+  {
+    title: 'Slide Image 2',
+    image: img2,
+  },
+  {
+    title: 'Slide Image 3',
+    image: img3,
+  },
+  {
+    title: 'Slide Image 4',
+    image: img4,
+  },
+];
+const ImageSlider = () => {
+  const options = {
+    type: 'loop',
+    autoplay: true,
+    // pauseOnHover: true,
+  };
+  return (
+    <Splide
+      hasTrack={false}
+      options={options}
+      tag='section'
+      className='h-full after:content-[""] after:absolute after:w-full after:h-full after:inset-0 after:bg-[#0000008e]'
+    >
+      <SplideTrack className='h-full'>
+        {images.map(({ title, image }) => {
+          return (
+            <SplideSlide key={title}>
+              <Image
+                src={image}
+                alt={title}
+                width={2400}
+                height={2400}
+                className='h-full object-cover object-center'
+              />
+            </SplideSlide>
+          );
+        })}
+      </SplideTrack>
+
+      <div className='splide__arrows hidden' />
+      <ul className='splide__pagination '></ul>
+    </Splide>
+  );
+};
+
+export default ImageSlider;
