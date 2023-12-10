5d39687e48fc1484772a8c86ff34159ead357ff5
Maged Faiz <magedfaiz98@gmail.com>
2023-12-11 00:38:40 +0200

    feat: refactored vehiclesGrid in catalogue

--- src/components/vehiclesCatalogue/vehiclesCatalogue.tsx
+++ src/components/vehiclesCatalogue/vehiclesCatalogue.tsx
@@ -1,9 +1,10 @@
 'use client';
 import React, { useState } from 'react';
-import SectionHeading from '../sectionHeading/SectionHeading';
-import { vehicle, vehicleType } from '@/types';
+import { AnimatePresence, motion } from 'framer-motion';
 import useFilterVehicles from '@/hooks/useFilterVehicles';
 import VehicleCard from '../vehicleCard/VehicleCard';
+import VehiclesFilter from './vehicleFilter';
+import { vehicle, vehicleType } from '@/types';
 
 const VehiclesCatalogue = ({
   vehicles,
@@ -16,65 +17,37 @@ const VehiclesCatalogue = ({
   const { filteredList } = useFilterVehicles(vehicles, selectedTypes);
   return (
     <div className='my-10 w-11/12 2xl:w-4/5 mx-auto flex flex-col xl:flex-row'>
-      <aside className='w-full xl:w-1/5'>
-        <SectionHeading Tag='h2' text='Filters' size='xl' />
-        <div className='divider -mt-2'></div>
-        <TypesSelector
-          types={types}
-          selected={selectedTypes}
-          setSelected={setSelectedType}
-        />
-      </aside>
-      <div className='w-full xl:w-4/5 grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
-        {filteredList.map((vehicle) => {
-          return <VehicleCard key={vehicle.slug.current} vehicle={vehicle} />;
-        })}
-      </div>
+      <VehiclesFilter
+        types={types}
+        count={filteredList.length}
+        selected={{ value: selectedTypes, setter: setSelectedType }}
+      />
+
+      <VehiclesGrid vehicles={filteredList} />
     </div>
   );
 };
 
 export default VehiclesCatalogue;
 
-type typesSelectorProps = {
-  types: vehicleType[];
-  selected: string[];
-  setSelected: React.Dispatch<React.SetStateAction<string[]>>;
-};
-
-const TypesSelector = ({
-  types,
-  selected,
-  setSelected,
-}: typesSelectorProps) => {
-  const handleChange = (title: string) => {
-    if (selected.includes(title)) {
-      setSelected(selected.filter((item) => item !== title));
-    } else {
-      setSelected([...selected, title]);
-    }
-  };
+const VehiclesGrid = ({ vehicles }: { vehicles: vehicle[] }) => {
   return (
-    <div className='collapse collapse-plus -mt-4'>
-      <input type='checkbox' />
-      <div className='collapse-title text-lg font-medium'>Vehicles</div>
-      <div className='collapse-content'>
-        {types.map(({ title }, index) => {
+    <div className='w-full xl:w-4/5 grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+      <AnimatePresence>
+        {vehicles.map((vehicle) => {
           return (
-            <div key={index} className='form-control'>
-              <label className='label justify-start gap-4 cursor-pointer'>
-                <input
-                  type='checkbox'
-                  className='checkbox'
-                  checked={selected.includes(title)}
-                  onChange={() => handleChange(title)}
-                />
-                <span className='label-text'>{title}</span>
-              </label>
-            </div>
+            <motion.div
+              key={vehicle.slug.current}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10 }}
+              className='h-full'
+            >
+              <VehicleCard vehicle={vehicle} />
+            </motion.div>
           );
         })}
-      </div>
+      </AnimatePresence>
     </div>
   );
 };
